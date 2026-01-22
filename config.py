aa4cc7359215f0f46ad0d7c42c64d37b93e77074
chapter-7-of-the-night <92494739+chapter-7-of-the-night@users.noreply.github.com>
2026-01-22 21:04:29 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 Cookies = [
-    'koa:sess=eyJ1c2VySWQiOjM4NTIwNCwiX2V4cGlyZSI6MTc5NDcxNDA4MDc0NiwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=glAHd7iOZ99ODDZ2Qm202HtmV4w'
+    'koa:sess=eyJ1c2VySWQiOjY3ODE5NCwiX2V4cGlyZSI6MTc5NDg3NjM0MTY2MSwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=lavNh_5xuPUNdlBmR8m1AJjNz2Q'
     ]
