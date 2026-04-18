a13173d8c6db48f270815863d0f3f79ae773ce37
chapter-7-of-the-night <92494739+chapter-7-of-the-night@users.noreply.github.com>
2026-04-18 09:00:16 +0800

    Update config.py
    修改cookies

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 Cookies = [
-    'koa:sess=eyJ1c2VySWQiOjY3ODE5NCwiX2V4cGlyZSI6MTc5NjYxMzQ1Nzk5OCwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=daA3sWXW1ABFlWdDO4pPLNiGFcE'
+    'koa:sess=eyJ1c2VySWQiOjY3ODE5NCwiX2V4cGlyZSI6MTc5NjYxMzQ1Nzk5OCwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=z4jY5vWZ4X8RMlWFS9Cm9kELWqg'
     ]
